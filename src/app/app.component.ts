1619295127c8c73be56ac723b8d3354a913519c3
Ahmet <130960696+AhmetIclier@users.noreply.github.com>
2023-08-21 10:55:48 +0200

    startet working on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ahmeticlier.de';
+export class AppComponent implements OnInit {
+  imgs = ['r1.png', 'r2.png', 'r3.png',]
+  currentImg = 0;
+  showImg = true;
+
+  ngOnInit() {
+    this.updateImage()
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImg++
+      this.currentImg = this.currentImg % this.imgs.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 1)
+    }, 3000)
+  }
+
+
 }
